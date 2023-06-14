9ee5918889d974207f5827a7be6c876ff3038b43
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-14 12:36:11 -0400

    Header y HeroSection responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ logoSrc, navItems }) => {
   return (
-     <header className="flex items-center justify-between px-12 bg-black md:py-4 md:px-24 ">
+     <header className="flex items-center justify-between px-12 bg-black md:py-4 md:px-24 lg:py-0 lg:px-35">
       <div className="flex items-center">
         <img src={logoSrc} alt="Logo" className="h-[5.5rem] w-[100px] md:h-[7.5rem] md:w-[150px] lg:h-[10.5rem] lg:w-[200px]" />
       </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import video from "../assets/test_video.mp4"
 const HeroSection = () => {
   return (
     <div className="hero-section w-full">
-      <video className="hero-video w-full h-auto min-h-[230px] object-cover" autoPlay muted loop>
+      <video className="hero-video w-full h-auto min-h-[230px] max-h-[760px] object-cover" autoPlay muted loop>
         <source src={video} type="video/mp4" />
          {/*Añade más etiquetas <source> aquí si deseas soporte para otros formatos de video */}
       </video>
