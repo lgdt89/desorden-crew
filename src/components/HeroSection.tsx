5404436b573f781710b5cf9a047a7bb888c1d33c
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-14 11:51:47 -0400

    home video modificado

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,10 @@
 import video from "../assets/test_video.mp4"
 const HeroSection = () => {
   return (
-    <div className="hero-section w-full h-[800px]">
-      <video className="hero-video w-full h-[800px] object-cover" autoPlay muted loop>
+    <div className="hero-section w-full">
+      <video className="hero-video w-full h-auto min-h-[230px] object-cover" autoPlay muted loop>
         <source src={video} type="video/mp4" />
-        {/* Añade más etiquetas <source> aquí si deseas soporte para otros formatos de video */}
+         {/*Añade más etiquetas <source> aquí si deseas soporte para otros formatos de video */}
       </video>
     </div>
   );
