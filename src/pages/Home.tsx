df87dc63d0f91da3b7d7faf5185c8514fc89c982
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-14 19:38:48 -0400

    Home modificado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div>
       <HeroSection />
-      <div className="w-full md:h-[700px] bg-white px-8 items-center flex flex-col md:flex-row justify-center items-center">
-        <div className="h-[200px] w-[200px] md:h-[400px] md:w-[400px] bg-[#fcb900] rounded-[50%] max-w-[500px] aspect-square flex justify-center items-center">
+      <div className="w-full md:h-[600px] bg-white px-0 md:px-5 lg:px-8 py-5 md:py-0 items-center flex flex-col md:flex-row justify-center items-center">
+        <div className="h-[200px] w-[200px] md:h-[360px] md:w-[360px] lg:h-[400px] lg:w-[400px] bg-[#fcb900] rounded-[50%] max-w-[500px] aspect-square flex justify-center items-center">
           <span className="absolute text-4xl md:text-8xl">
             No <br />
             Star-
@@ -16,15 +16,15 @@ const Home = () => {
             Dom
           </span>
         </div>
-        <div className="ml-8 w-auto pl-12">
+        <div className="w-auto p-9 md:ml-20 text-justify md:text-left">
           <div>
-            <h1 className="text-[40px] text-yellow-500">
+            <h1 className="text-[20px] md:[25px] lg:text-[30px] text-yellow-500">
               We believe in creativity that awards
               <br /> businesses with results
             </h1>
           </div>
           <div>
-            <p className="text-[24px] font-light">
+            <p className="text-[15spx] md:[20px] lg:text-[25px] font-light">
               Lemon is astrategic, creative and proactive advertising and <br />{" "}
               communications agency that helps brands and companies grow.
               <br />
