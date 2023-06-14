e6e75538be53988d9cc2537c16a3617163c216da
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-14 18:17:50 -0400

    Services modificados

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -7,11 +7,11 @@ const ServiceCard = ({ image, description, customCLass }: ServiceCardProps) => {
   return (
     <div className="flex flex-col  items-center">
       <div
-        className={`h-[250px] w-[250px] rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:duration-300 ${customCLass}`}
+        className={`h-[230px] w-[230px] rounded-full transition ease-in-out delay-125 hover:-translate-y-1 hover:scale-125 hover:duration-200 ${customCLass}`}
       >
         {image}
       </div>
-      <p className="self-center mt-8 text-3xl text-center">{description}</p>
+      <p className="self-center mt-8 text-[18px] md:text-[22px] lg:text-[25px] text-center">{description}</p>
     </div>
   );
 };
--- src/views/Services.tsx
+++ src/views/Services.tsx
@@ -2,7 +2,7 @@ import ServiceCard from '../components/ServiceCard'
 
 const Services = () => {
   return (
-    <div className="h-auto md:h-[900px] pt-[100px] pb-[100px] w-full bg-gray-200 grid grid-cols-1 md:grid-cols-3 items-center place-items-center">
+    <div className="w-full h-auto pt-[100px] pb-[100px] bg-gray-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 md:gap-10 items-center place-items-center">
       <ServiceCard customCLass='bg-yellow-300' description='Brand & Communication Strategy'/>
       <ServiceCard customCLass='bg-red-300' description='Advertising'/>
       <ServiceCard customCLass='bg-amber-300' description='Digital & Social Media'/>
