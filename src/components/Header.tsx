032f7c2a897fca5c25598a9a13a225130de09c55
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-14 11:25:17 -0400

    Logo responsive modificado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = ({ logoSrc, navItems }) => {
   return (
      <header className="flex items-center justify-between px-12 bg-black md:py-4 md:px-24 ">
       <div className="flex items-center">
-        <img src={logoSrc} alt="Logo" className="h-[5.5rem] w-[100px] md:h-[10.5rem] md:w-[300px]" />
+        <img src={logoSrc} alt="Logo" className="h-[5.5rem] w-[100px] md:h-[7.5rem] md:w-[150px] lg:h-[10.5rem] lg:w-[200px]" />
       </div>
       <Navbar items={navItems} />
       
