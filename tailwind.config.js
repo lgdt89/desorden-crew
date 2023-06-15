7be4d9e3ae25a61215090662a3d90c9c60c7633a
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-15 14:39:54 -0400

    Plugin tailwind agregado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+const backfaceVisibility = plugin(function({addUtilities}) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    '.backface-visible': {
+      'backface-visibility': 'visible',
+      '-moz-backface-visibility': 'visible',
+      '-webkit-backface-visibility': 'visible',
+      '-ms-backface-visibility': 'visible'
+    },
+    '.backface-hidden': {
+      'backface-visibility': 'hidden',
+      '-moz-backface-visibility': 'hidden',
+      '-webkit-backface-visibility': 'hidden',
+      '-ms-backface-visibility': 'hidden'
+    }
+  })
+});
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +35,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    backfaceVisibility
+  ],
 }
 
