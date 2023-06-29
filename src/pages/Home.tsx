006feba47e99e917d843059ab1b8f6c7694b9f8e
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-06-29 17:38:17 -0400

    Home text section modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <div>
       <HeroSection />
-      <div className="w-full md:h-[500px] bg-[#dfece6] px-0 md:px-5 lg:px-8 py-5 md:py-0 items-center flex flex-col md:flex-row justify-center items-center">
-        <div className="w-[50%] h-[100%] items-center text-[40px] flex justify-center md:justify-end md:text-[70px] px-[20px]">
-          <h1 className="text-center md:text-left mb-[20px]">
+      <div className="w-full h-full md:h-[540px] bg-[#dfece6] px-0 py-14 md:px-5 lg:px-8 md:py-0 items-center flex flex-col md:flex-row justify-center items-center gap-x-6">
+        <div className="w-[90%] h-[100%] items-center flex justify-center ">
+          <h1 className="text-left mb-[25px] px-8 text-[40px] md:text-[50px] lg:text-[60px]">
             {" "}
             If <br />
             you're going <br />
@@ -16,16 +16,15 @@ const Home = () => {
             <br /> do it right.
           </h1>
         </div>
-        <div className="w-[50%] h-[100%] flex flex-col justify-center  px-[20px]">
+        <div className="w-[90%] h-[100%] text-justify flex flex-col justify-center  px-[20px]">
           {" "}
-          <p className="font-primary text-center md:text-justify text-[20px] lg:text-[30px] text-yellow-500">
+          <p className="font-primary pb-[10px] text-[25px] lg:text-[35px] text-yellow-500">
             It is proven that creativity is closely
-            <br /> linked to positive business results.
+            linked to positive business results.
           </p>
-          <p className="font-secondary text-center md:text-justify text-[15spx] md:[20px] lg:text-[25px] font-light">
-            Desorden is a Boutique advertising <br /> and strategic
+          <p className="font-secondary text-[18px] md:[20px] lg:text-[25px] font-light">
+            Desorden is a Boutique advertising and strategic
             communications agency with a creative
-            <br />
             and proactive approach that helps brands and companies grow
           </p>
         </div>
